9037112aef314053d7ef769aefbc9226571b0a90
Ana Paula Vale <apvalefin@gmail.com>
2021-11-10 22:42:53 +0000

    feat(Home): Implement home page and navbar

--- pages/index.js
+++ pages/index.js
@@ -1,82 +1,38 @@
-import Head from 'next/head'
+import Head from "next/head";
+import Image from "next/image";
+import Footer from "../components/Footer";
+import Navbar from "../components/Navbar";
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+    <div className="container mx-auto">
       <Head>
-        <title>Create Next App</title>
+        <title>Mulheres de Produto</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <Navbar />
 
-      <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
-
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="p-3 font-mono text-lg bg-gray-100 rounded-md">
-            pages/index.js
-          </code>
-        </p>
-
-        <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+      <main className="flex flex-col items-center justify-center h-screen text-center">
+        <div className="relative tracking-normal m-2 max-w-full z-50 text-white text-6xl font-semibold leading-normal mr-0 whitespace-pre-line break-words">
+          MULHERES DE PRODUTO
+        </div>
+        <span className="min-w-full text-xl break-words text-white z-50 font-normal normal-case w-50">
+          Uma comunidade de mentoria e crescimento profissional para todas as
+          mulheres nas áreas de criação de produtos digitais.
+        </span>
+        <div className="opacity-90">
+          <Image
+            src="/image.png"
+            alt="Foto evento Mulheres de Produto"
+            width={1431}
+            height={1074}
+            objectFit="cover"
+            quality={100}
+            layout="fill"
+          />
         </div>
       </main>
-
-      <footer className="flex items-center justify-center w-full h-24 border-t">
-        <a
-          className="flex items-center justify-center"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="h-4 ml-2" />
-        </a>
-      </footer>
+      <Footer />
     </div>
-  )
+  );
 }
---
+++ pages/quem-somos.js
@@ -0,0 +1,16 @@
+import Footer from "../components/Footer";
+import Navbar from "../components/Navbar";
+
+export default function QuemSomos() {
+  return (
+    <div className="container mx-auto">
+      <Navbar />
+
+      <main className="flex flex-col items-center justify-center h-screen text-center">
+        Página em construção
+        <br /> Quem somos
+      </main>
+      <Footer />
+    </div>
+  );
+}
