ff115736f309562c5b1ccfd0ba5d356c1070c93f
Kete Rufino <kete.rufino@gympass.com>
2022-02-01 21:29:01 -0300

    adding config to enable progressive migration from the old to new website infra

--- next.config.js
+++ next.config.js
@@ -11,4 +11,18 @@ module.exports = {
   images: {
     domains: ['images.ctfassets.net'],
   },
+  async rewrites() {
+    return [
+      // we need to define a no-op rewrite to trigger checking
+      // all pages/static files before we attempt proxying
+      {
+        source: '/:path*',
+        destination: '/:path*',
+      },
+      {
+        source: '/:path*',
+        destination: `https://mulheresdeproduto.com/:path*`,
+      },
+    ]
+  },
 }
